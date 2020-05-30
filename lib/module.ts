9cb181991691a8212d67553d247a0ad049afffa7
gjuoun <gjuoun@gmail.com>
2020-05-30 04:23:29 -0300

    update module

--- lib/module.ts
+++ lib/module.ts
@@ -1,4 +1,4 @@
 // @deno-types="../node_modules/@types/react/index.d.ts"
-import {default as React} from "https://dev.jspm.io/react@16.13.1/index.js";
+export {default as React} from "https://dev.jspm.io/react@16.13.1/index.js";
 // @deno-types="../node_modules/@types/react-dom/server/index.d.ts"
-import {default as ReactDOMServer} from "https://dev.jspm.io/react-dom@16.13.1/server.js";
+export {default as ReactDOMServer} from "https://dev.jspm.io/react-dom@16.13.1/server.js";
