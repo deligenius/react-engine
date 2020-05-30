294c97558a5a0a63673db15c9f1452e5182767c6
gjuoun <gjuoun@gmail.com>
2020-05-30 04:25:51 -0300

    move module to mod.ts

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-// @deno-types="../node_modules/@types/react/index.d.ts"
+// @deno-types="./node_modules/@types/react/index.d.ts"
 export {default as React} from "https://dev.jspm.io/react@16.13.1/index.js";
-// @deno-types="../node_modules/@types/react-dom/server/index.d.ts"
+// @deno-types="./node_modules/@types/react-dom/server/index.d.ts"
 export {default as ReactDOMServer} from "https://dev.jspm.io/react-dom@16.13.1/server.js";
